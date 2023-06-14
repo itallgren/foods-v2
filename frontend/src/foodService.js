c356023acef1f0208a026537bc66ee7438eccb32
Ida Tallgren <ida.tallgren@hotmail.com>
2023-06-14 10:38:53 +0300

    Add env variable to frontend

--- frontend/src/foodService.js
+++ frontend/src/foodService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/api/";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getRandomFood = async () => {
   return await axios.get(API_URL + "foods/random");
